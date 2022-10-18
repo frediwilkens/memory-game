a753ce5942d426f8aef8fcb6b949253d4e15e9c7
Fredi Wilkens <frediwilkens@gmail.com>
2022-10-17 22:25:00 -0300

    feat: add pokemons data to App component

--- src/templates/App/index.tsx
+++ src/templates/App/index.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
+import Card, { CardProps } from '../../components/Card';
+import fetchPokemon from '../../utils/pokeApi';
+import { cardsManipulation } from '../../utils/cards-manipulation';
 import './styles.css';
 
 function App() {
+  const handleClick = (id: any) => console.log(id);
+  const [data, setData] = useState(() => [] as CardProps[]);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const pokeArray = [];
+      for (let index = 1; index <= 4; index += 1) {
+        const pokemon: CardProps = await fetchPokemon(index);
+        pokeArray.push(pokemon);
+      }
+      setData(pokeArray);
+      setData((prev) => cardsManipulation(prev));
+    }
+    getPokemons();
+  },[]);
+
+  console.log(data);
+
   return (
-    <h1>Oi</h1>
+    data.map((card) => (
+      <Card
+        key={card.id}
+        id={card.id}
+        pokeName={card.pokeName}
+        pokeImage={card.pokeImage}
+        flipped
+        handleClick={handleClick}
+      />
+    ))    
   )
 }
 
